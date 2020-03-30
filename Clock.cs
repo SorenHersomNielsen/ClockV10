215a3161810494d0f88bd2578f561708887556b1
SorenHersomNielsen <hersom@ellekrattet.dk>
2020-03-30 13:46:59 +0200

    Initial commit

---
+++ Clock.cs
@@ -0,0 +1,18 @@
+﻿namespace ClockV10
+{
+    public class Clock
+    {
+      private int timeinmin;
+      private int min;
+     
+      
+public void Settime(int hours, int min)
+{
+timeinmin = 60 * hours + min;
+}
+public void plus()
+{
+    min = min + 1;
+}
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClockV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+Clock myclock = new Clock();
+myclock.Settime(14,45);
+myclock.plus();
+System.Console.WriteLine();
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
